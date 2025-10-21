cefd5a05f59605e9f7fe155657e21d784d057b46
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-21 10:46:10 -0300

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -5,25 +5,24 @@ public class Q1 {
         Scanner read = new Scanner(System.in);
         int n1, n2, n3, n4;
 
-        System.out.println("Nota do 1° Bimestre: ");
+        System.out.println("\u001b[34mNota do 1° Bimestre: ");
         n1 = read.nextInt();
-        System.out.println("Nota do 2° Bimestre: ");
+        System.out.println("\u001b[32mNota do 2° Bimestre: ");
         n2 = read.nextInt();
-        System.out.println("Nota do 3° Bimestre: ");
+        System.out.println("\u001b[33mNota do 3° Bimestre: ");
         n3 = read.nextInt();
-        System.out.println("Nota do 4° Bimestre: ");
+        System.out.println("\u001b[36mNota do 4° Bimestre: ");
         n4 = read.nextInt();
 
         int media = (n1 + n2 + n3 + n4) / 4;
-
         if (media >= 60 && media <= 100) {
-            System.out.println(media + ". Aprovado.");
+            System.out.println("\u001b[35mMédia: " + media + ". Aprovado.");
         } else if (media < 60 && media >= 40) {
-            System.out.println(media + ". Prova final.");
+            System.out.println("\u001b[35mMédia: " +media + ". Prova final.");
         } else if (media < 40) {
-            System.out.println(media + ". Reprovado.");
+            System.out.println("\u001b[35mMédia: " + media + ". Reprovado.");
         } else {
-            System.out.println("Inválido.");
+            System.out.println("Inválido.\u001b[0m");
         }
         read.close();
     }
