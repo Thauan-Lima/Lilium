d58f3dab159fca3ef3985cf44b1d9f3e318a438c
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-22 16:13:53 -0300

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int raio;
+
+        System.out.println("\u001B[33mDê o raio de um círculo: ");
+        raio = read.nextInt();
+
+        int diam = 2*raio;
+        double area = Math.PI*raio*raio;
+
+        System.out.println("\u001B[31mDiâmetro: "+ diam);
+        System.out.println("\u001B[32mÁrea: "+ area);
+        read.close();
+    }
+}
