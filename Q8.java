aaade107f43763ab17b8b342119f3288d9244a3f
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-23 14:10:14 -0300

    Create Q8.java

---
+++ Q8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        double valor;
+        int horas;
+        System.out.println("\u001B[34mValor por hora: ");
+        valor = read.nextDouble();
+
+        System.out.println("\u001B[32mHoras de trabalho no mês: ");
+        horas = read.nextInt();
+
+        double salarioBruto = valor * horas;
+        double impostoRenda = (salarioBruto * 11)/100;
+        double INSS = (salarioBruto * 8)/100;
+        double sindicato = (salarioBruto * 5)/100;
+        double salarioLiq = salarioBruto - (impostoRenda + INSS + sindicato);
+        System.out.println("\u001B[35mSalário Bruto: " + salarioBruto);
+        System.out.println("\u001B[35mQuanto pagou ao INSS: " + INSS);
+        System.out.println("\u001B[35mQuanto pagou ao sindicato: " + sindicato);
+        System.out.println("\u001B[35mO salário líquido: " + salarioLiq + "\u001b[0m");
+        read.close();
+    }
+}
