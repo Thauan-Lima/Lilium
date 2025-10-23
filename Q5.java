bbdc4f049d29ebc4e8b6fc04d4bc85c2ef6cf9c1
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-23 13:38:55 -0300

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -8,16 +8,17 @@ public class Q5 {
 
         System.out.println("\u001B[33mDigite o primeiro número (inteiro): ");
         n1 = read.nextInt();
-        System.out.println("\u001B[33mDigite o segundo número (inteiro): ");
+        System.out.println("\u001B[32mDigite o segundo número (inteiro): ");
         n2 = read.nextInt();
-         System.out.println("\u001B[33mDigite o terceiro número (real): ");
+         System.out.println("\u001B[34mDigite o terceiro número (real): ");
         n3 = read.nextDouble();
 
         a = (2*n1)*(n2/2);
         b = 3*n1 + n3;
         c = n3*n3*n3;
-
-
+        System.out.println("\u001b[35mO produto do dobro do primeiro com metade do segundo é: " + a);
+        System.out.println("\u001b[35mA soma do triplo do primeiro com o terceiro é: " + b);
+        System.out.println("\u001b[35mO terceiro elevado ao cubo é: " + c + "\u001b[0m");
         read.close();
     }
 }
