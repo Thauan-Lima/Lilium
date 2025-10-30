2f38332b361f572c53fa645ed6cc1e3422b1d135
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-30 17:47:54 -0300

    Create Q17.java

---
+++ Q17.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Lanchonete {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double total = 0.0;
+        int codigo, quantidade;
+
+        System.out.println("===== CARDÁPIO =====");
+        System.out.println("100 - Cachorro-quente   R$ 1,20");
+        System.out.println("101 - Bauru simples     R$ 1,30");
+        System.out.println("102 - Bauru com ovo     R$ 1,50");
+        System.out.println("103 - Hambúrguer        R$ 1,20");
+        System.out.println("104 - Cheeseburguer     R$ 1,30");
+        System.out.println("105 - Refrigerante      R$ 1,00");
+        System.out.println("======================");
+        System.out.println("Digite 0 para encerrar o pedido.\n");
+
+        while (true) {
+            System.out.print("Digite o código do produto: ");
+            codigo = scanner.nextInt();
+
+            if (codigo == 0) {
+                break; // encerra o pedido
+            }
+
+            System.out.print("Digite a quantidade: ");
+            quantidade = scanner.nextInt();
+
+            double preco = 0;
+
+            switch (codigo) {
+                case 100:
+                    preco = 1.20;
+                    break;
+                case 101:
+                    preco = 1.30;
+                    break;
+                case 102:
+                    preco = 1.50;
+                    break;
+                case 103:
+                    preco = 1.20;
+                    break;
+                case 104:
+                    preco = 1.30;
+                    break;
+                case 105:
+                    preco = 1.00;
+                    break;
+                default:
+                    System.out.println("Código inválido! Tente novamente.");
+                    continue; // volta ao início do loop
+            }
+
+            total += preco * quantidade;
+        }
+
+        System.out.printf("\nValor total do pedido: R$ %.2f%n", total);
+
+        scanner.close();
+    }
+}
