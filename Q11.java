77e8cedc798f9f9e031a6f74c004238811800ed1
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-30 08:32:42 -0300

    Update Q11.java

--- Q11.java
+++ Q11.java
@@ -1,4 +1,14 @@
-if (salario <= 280.00) {
+import java.util.Scanner;
+
+public class Q11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double salario, percentual, aumento, novoSalario;
+        
+        System.out.print("\u001b[31mInforme o salário atual do colaborador:\nR$ ");
+        salario = input.nextDouble();
+
+        if (salario <= 280.00) {
             percentual = 20.0;
         } else if (salario <= 700.00) {
             percentual = 15.0;
@@ -7,3 +17,16 @@ if (salario <= 280.00) {
         } else {
             percentual = 5.0;
         }
+
+        aumento = salario * (percentual / 100);
+        novoSalario = salario + aumento;
+
+        System.out.println("\u001b[1m\u001b[35mResultado do Reajuste");
+        System.out.printf("Salário antes do reajuste: R$ %.2f%n", salario);
+        System.out.printf("Percentual de aumento aplicado: %.0f%%%n", percentual);
+        System.out.printf("Valor do aumento: R$ %.2f%n", aumento);
+        System.out.printf("Novo salário após o aumento: R$ %.2f%n\u001b[0m", novoSalario);
+
+        input.close();
+    }
+}
