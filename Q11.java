61ccd636fbdaeb8262b37bf29cb38ddd699a33aa
Thauan-Lima <oliveira.thauan@escolar.ifrn.edu.br>
2025-10-30 08:21:58 -0300

    Create Q11.java

---
+++ Q11.java
@@ -0,0 +1,9 @@
+if (salario <= 280.00) {
+            percentual = 20.0;
+        } else if (salario <= 700.00) {
+            percentual = 15.0;
+        } else if (salario <= 1500.00) {
+            percentual = 10.0;
+        } else {
+            percentual = 5.0;
+        }
